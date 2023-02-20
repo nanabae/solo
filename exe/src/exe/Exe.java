907a27d2aab34afbdc3272087f51b6dcf6cb961d
DESKTOP-5KUC7AT\user2 <baenana2023@gamil.com>
2023-02-20 21:06:07 +0900

    ㅋㅁ

--- exe/src/exe/Exe.java
+++ exe/src/exe/Exe.java
@@ -3,8 +3,7 @@ package exe;
 public class Exe {
 
 	public static void main(String[] args) {
-		System.out.println("연습! 집에서 잘 받아질까?");
-		System.out.println("잘 받기");
+		System.out.println("집으로");
 	}
 
 }
