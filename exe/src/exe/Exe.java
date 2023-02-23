316b03f7db5aedcce0341104bded701f637c0d5c
DESKTOP-5KUC7AT\user2 <baenana2023@gamil.com>
2023-02-23 17:59:02 +0900

    Revert "포문작성"
    This reverts commit d4818902849a83d7e029820e012901bcaee90b03.

--- exe/src/exe/Exe.java
+++ exe/src/exe/Exe.java
@@ -5,10 +5,6 @@ public class Exe {
 	public static void main(String[] args) {
 		System.out.println("집으로");
 		System.out.println("02.23");
-		for(int i=0;i<10;i++) {
-			System.out.println(i);
-		}
 	}
-	
 
 }
